2cf280d500acfcbe9b04a201c90e72401be78dbf
rbqks529 <rbqks529@naver.com>
2025-09-23 14:15:50 +0900

    [feat]: Auth 네비게이션 구현 (#12)

--- composeApp/src/commonMain/kotlin/org/whosin/client/core/navigation/WhosInNavGraph.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/core/navigation/WhosInNavGraph.kt
@@ -6,7 +6,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import org.whosin.client.presentation.auth.LoginScreen
+import org.whosin.client.presentation.auth.clubcode.ClubCodeInputScreen
+import org.whosin.client.presentation.auth.login.EmailVerificationScreen
+import org.whosin.client.presentation.auth.login.LoginScreen
+import org.whosin.client.presentation.auth.login.NicknameInputScreen
+import org.whosin.client.presentation.auth.login.FindPasswordScreen
+import org.whosin.client.presentation.auth.login.PasswordInputScreen
+import org.whosin.client.presentation.auth.login.SignupScreen
+import org.whosin.client.presentation.auth.login.SplashScreen
 import org.whosin.client.presentation.home.HomeScreen
 import org.whosin.client.presentation.mypage.MyPageScreen
 
@@ -21,13 +28,95 @@ fun WhosInNavGraph(
     ) {
         /* 인증,인가 화면들을 위한 별도의 graph */
         navigation<Route.AuthGraph>(
-            startDestination = Route.Login,
+            startDestination = Route.Splash,
         ) {
+            composable<Route.Splash> {
+                SplashScreen(
+                    modifier = modifier,
+                    onNavigateToLogin = {
+                        navController.navigate(Route.Login) {
+                            popUpTo(Route.Splash) { inclusive = true }
+                        }
+                    }
+                )
+            }
+            
             composable<Route.Login> {
                 LoginScreen(
                     modifier = modifier,
                     onNavigateToHome = {
                         navController.navigate(Route.Home)
+                    },
+                    onNavigateToFindPassword = {
+                        navController.navigate(Route.FindPassword)
+                    },
+                    onNavigateToSignup = {
+                        navController.navigate(Route.Signup)
+                    }
+                )
+            }
+            
+            composable<Route.FindPassword> {
+                FindPasswordScreen(
+                    modifier = modifier,
+                    onNavigateBack = { navController.navigateUp() },
+                    onPasswordResetComplete = {
+                        navController.navigate(Route.Login) {
+                            popUpTo(Route.FindPassword) { inclusive = true }
+                        }
+                    }
+                )
+            }
+            
+            composable<Route.Signup> {
+                SignupScreen(
+                    modifier = modifier,
+                    onNavigateBack = { navController.navigateUp() },
+                    onNavigateToEmailVerification = { email ->
+                        navController.navigate(Route.EmailVerification)
+                    }
+                )
+            }
+            
+            composable<Route.EmailVerification> { backStackEntry ->
+                
+                EmailVerificationScreen(
+                    modifier = modifier,
+                    onNavigateBack = { navController.navigateUp() },
+                    onVerificationComplete = {
+                        navController.navigate(Route.PasswordInput)
+                    }
+                )
+            }
+            
+            composable<Route.PasswordInput> {
+                PasswordInputScreen(
+                    modifier = modifier,
+                    onNavigateBack = { navController.navigateUp() },
+                    onPasswordComplete = { password, confirmPassword ->
+                        navController.navigate(Route.NicknameInput)
+                    }
+                )
+            }
+            
+            composable<Route.NicknameInput> {
+                NicknameInputScreen(
+                    modifier = modifier,
+                    onNavigateBack = { navController.navigateUp() },
+                    onNavigateToClubCode = {
+                        navController.navigate(Route.ClubCodeInput)
+                    }
+                )
+            }
+            
+            composable<Route.ClubCodeInput> {
+                ClubCodeInputScreen(
+                    modifier = modifier,
+                    onNavigateBack = { navController.navigateUp() },
+                    onNavigateToHome = {
+                        navController.navigate(Route.Home) {
+                            popUpTo(Route.AuthGraph) { inclusive = true }
+                        }
                     }
                 )
             }
