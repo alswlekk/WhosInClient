ff0eb96dd5040b2529ff077b6516fac0f3e3660b
rbqks529 <rbqks529@naver.com>
2025-10-02 17:30:47 +0900

    [feat]: 이메일 인증 로직 DataSource에 구현 (#26)

--- composeApp/src/commonMain/kotlin/org/whosin/client/data/remote/RemoteMemberDataSource.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/data/remote/RemoteMemberDataSource.kt
@@ -2,16 +2,16 @@ package org.whosin.client.data.remote
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.isSuccess
 import org.whosin.client.core.network.ApiResult
-import org.whosin.client.data.dto.request.LoginRequestDto
+import org.whosin.client.data.dto.request.EmailValidationRequestDto
 import org.whosin.client.data.dto.request.EmailVerificationRequestDto
-import org.whosin.client.data.dto.response.LoginResponseDto
+import org.whosin.client.data.dto.request.LoginRequestDto
 import org.whosin.client.data.dto.response.EmailVerificationResponseDto
+import org.whosin.client.data.dto.response.LoginResponseDto
 
 class RemoteMemberDataSource(
     private val client: HttpClient
@@ -46,6 +46,31 @@ class RemoteMemberDataSource(
                 .post("api/auth/email/send") {
                     setBody(
                         EmailVerificationRequestDto(email = email)
+                    )
+                }
+            if (response.status.isSuccess()) {
+                ApiResult.Success(
+                    data = response.body(),
+                    statusCode = response.status.value
+                )
+            } else {
+                val errorResponse: EmailVerificationResponseDto = response.body()
+                ApiResult.Error(
+                    code = errorResponse.status,
+                    message = errorResponse.message
+                )
+            }
+        } catch (t: Throwable) {
+            ApiResult.Error(message = t.message, cause = t)
+        }
+    }
+
+    suspend fun validateEmailCode(email: String, authCode: String): ApiResult<EmailVerificationResponseDto> {
+        return try {
+            val response: HttpResponse = client
+                .post("api/auth/email/validation") {
+                    setBody(
+                        EmailValidationRequestDto(email = email, authCode = authCode)
                     )
                 }
             if (response.status.isSuccess()) {
