de086049ea0b89089f45ae4cdc7564be22f64f5c
rbqks529 <rbqks529@naver.com>
2025-10-02 16:58:20 +0900

    [feat]: DataSource에 이메일 인증 함수 추가 (#26)

--- composeApp/src/commonMain/kotlin/org/whosin/client/data/remote/RemoteMemberDataSource.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/data/remote/RemoteMemberDataSource.kt
@@ -9,7 +9,9 @@ import io.ktor.client.statement.HttpResponse
 import io.ktor.http.isSuccess
 import org.whosin.client.core.network.ApiResult
 import org.whosin.client.data.dto.request.LoginRequestDto
+import org.whosin.client.data.dto.request.EmailVerificationRequestDto
 import org.whosin.client.data.dto.response.LoginResponseDto
+import org.whosin.client.data.dto.response.EmailVerificationResponseDto
 
 class RemoteMemberDataSource(
     private val client: HttpClient
@@ -17,8 +19,7 @@ class RemoteMemberDataSource(
     suspend fun login(email: String, password: String): ApiResult<LoginResponseDto> {
         return try {
             val response: HttpResponse = client
-                // TODO: BaseUrl 가져올 수 있도록 처리
-                .post(urlString = "BASEURL/members/login") {
+                .post("api/members/login") {
                     setBody(
                         LoginRequestDto(email = email, password = password)
                     )
@@ -32,6 +33,31 @@ class RemoteMemberDataSource(
                 ApiResult.Error(
                     code = response.status.value,
                     message = "HTTP ${response.status.value}"
+                )
+            }
+        } catch (t: Throwable) {
+            ApiResult.Error(message = t.message, cause = t)
+        }
+    }
+
+    suspend fun sendEmailVerification(email: String): ApiResult<EmailVerificationResponseDto> {
+        return try {
+            val response: HttpResponse = client
+                .post("api/auth/email/send") {
+                    setBody(
+                        EmailVerificationRequestDto(email = email)
+                    )
+                }
+            if (response.status.isSuccess()) {
+                ApiResult.Success(
+                    data = response.body(),
+                    statusCode = response.status.value
+                )
+            } else {
+                val errorResponse: EmailVerificationResponseDto = response.body()
+                ApiResult.Error(
+                    code = errorResponse.status,
+                    message = errorResponse.message
                 )
             }
         } catch (t: Throwable) {
