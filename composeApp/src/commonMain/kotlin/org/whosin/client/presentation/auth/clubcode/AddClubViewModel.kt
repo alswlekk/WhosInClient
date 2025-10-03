a1c8c25f5c4816c812c3cf7b38d820cb02d22918
casperjr <hyunjw0515@gmail.com>
2025-10-03 21:58:38 +0900

    [CHORE]: 동아리 추가 화면 viewmodel 기본 틀 작성

---
+++ composeApp/src/commonMain/kotlin/org/whosin/client/presentation/auth/clubcode/AddClubViewModel.kt
@@ -0,0 +1,12 @@
+package org.whosin.client.presentation.auth.clubcode
+
+import org.whosin.client.data.repository.ClubRepository
+
+class AddClubViewModel(
+    private val repository: ClubRepository
+) {
+
+    // TODO: 동아리 번호 확인 함수
+
+    // TODO: 동아리 추가 함수
+}
