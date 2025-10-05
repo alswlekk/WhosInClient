f2410864d4fda169b6200f7f029408a57a576b62
rbqks529 <rbqks529@naver.com>
2025-10-05 16:56:09 +0900

    [refactor]: 기존 코드의 중복 제거 (#26)

--- composeApp/src/commonMain/kotlin/org/whosin/client/data/remote/RemoteMemberDataSource.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/data/remote/RemoteMemberDataSource.kt
@@ -4,46 +4,18 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.patch
-import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.isSuccess
 import org.whosin.client.core.network.ApiResult
-import org.whosin.client.data.dto.request.LoginRequestDto
 import org.whosin.client.data.dto.request.UpdateMyInfoRequestDto
 import org.whosin.client.data.dto.response.ErrorResponseDto
-import org.whosin.client.data.dto.response.LoginResponseDto
 import org.whosin.client.data.dto.response.MyInfoResponseDto
 import org.whosin.client.data.dto.response.UpdateMyInfoResponseDto
 
 class RemoteMemberDataSource(
     private val client: HttpClient
 ) {
-    suspend fun login(email: String, password: String): ApiResult<LoginResponseDto> {
-        return try {
-            val response: HttpResponse = client
-                // TODO: BaseUrl 가져올 수 있도록 처리
-                .post(urlString = "BASEURL/members/login") {
-                    setBody(
-                        LoginRequestDto(email = email, password = password)
-                    )
-                }
-            if (response.status.isSuccess()) {
-                ApiResult.Success(
-                    data = response.body(),
-                    statusCode = response.status.value
-                )
-            } else {
-                ApiResult.Error(
-                    code = response.status.value,
-                    message = "HTTP ${response.status.value}"
-                )
-            }
-        } catch (t: Throwable) {
-            ApiResult.Error(message = t.message, cause = t)
-        }
-    }
-
     // 내 정보 조회
     suspend fun getMyInfo(): ApiResult<MyInfoResponseDto> {
         return try {
--- composeApp/src/commonMain/kotlin/org/whosin/client/data/repository/MemberRepository.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/data/repository/MemberRepository.kt
@@ -2,20 +2,15 @@ package org.whosin.client.data.repository
 
 import org.whosin.client.data.remote.RemoteMemberDataSource
 import org.whosin.client.core.network.ApiResult
-import org.whosin.client.data.dto.response.LoginResponseDto
 import org.whosin.client.data.dto.response.MyInfoResponseDto
 import org.whosin.client.data.dto.response.UpdateMyInfoResponseDto
 
 class MemberRepository(
     private val dataSource: RemoteMemberDataSource
 ) {
-    suspend fun login(email: String, password: String): ApiResult<LoginResponseDto> =
-        dataSource.login(email, password)
-
     suspend fun getMyInfo(): ApiResult<MyInfoResponseDto> =
         dataSource.getMyInfo()
 
     suspend fun updateMyInfo(newNickName: String, clubList: List<Int>?): ApiResult<UpdateMyInfoResponseDto> =
         dataSource.updateMyInfo(newNickName = newNickName, clubList = clubList)
-
 }
