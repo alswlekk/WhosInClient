64150017a2db369f48fc7e098787f0d94092a484
casperjr <hyunjw0515@gmail.com>
2025-10-12 21:43:28 +0900

    [CHOER]: 회원 탈퇴 dto 생성

---
+++ composeApp/src/commonMain/kotlin/org/whosin/client/data/dto/response/DeleteAccountResponseDto.kt
@@ -0,0 +1,16 @@
+package org.whosin.client.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DeleteAccountResponseDto(
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: String? = null
+)
